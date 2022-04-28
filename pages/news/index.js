24acd6b52797fb4560178676df52c7f6becd88e7
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-04-28 21:30:48 +0600

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { Fragment } from "react";
 // our-domain.com/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
